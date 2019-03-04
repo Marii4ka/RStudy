600cf241f3b592e2bc408c18ac9684a1c86e9f61
Marii4ka <mariakolyada@gmail.com>
2019-03-04 20:09:37 +0000

    new note

--- code/raw_code/script_test_code.R
+++ code/raw_code/script_test_code.R
@@ -1,3 +1,18 @@
 mpg <- mtcars$mpg
 summary(mpg)
 hist(mpg)
+
+#let's create a few object!
+mpg <- mtcars$mpg
+cyl <- mtcars$cyl
+
+save(mpg, cyl, file="mtcars_objects.rda")
+
+load("mtcars_objects.rda")
+
+saveRDS(mpg, file="mpg.rds")
+mpg <- readRDS("mpg.rds")
+readRDS("mpg.rds")
+
+
+
